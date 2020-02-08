b07a09e8f41ebf4b8b8b17aedc4e4e06021fd7e4
Rick Middleton <ricksteruk@mac.com>
2020-02-08 00:35:12 +0000

    Updates

---
+++ precache-manifest.fafad0a58303c939722f716a0757fbae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50c520c7a96ae7fdb97b0678d7e7b43",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8a66c2f1c71d9024cd2d",
+    "url": "/monsters-rolodex/static/css/main.7d5bf0ce.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a66c2f1c71d9024cd2d",
+    "url": "/monsters-rolodex/static/js/main.0241e635.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
